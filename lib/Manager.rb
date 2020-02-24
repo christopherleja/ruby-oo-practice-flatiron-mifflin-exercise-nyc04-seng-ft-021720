5446039e6cbf3c1d633a347e4030a47f5850c0e3
christopherleja <christopher.leja@gmail.com>
2020-02-24 14:03:16 -0500

    done

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,40 @@
 class Manager
-
-end
+  
+    attr_accessor :name, :department, :age
+    
+    @@all = []
+    
+    def initialize(name, department, age)
+      @name = name
+      @department = department
+      @age = age
+      @@all << self
+    end
+  
+    def employees
+      Employee.all.select { |employee| employee.manager == self} 
+    end
+    
+    def hire_employee(name, salary)
+      Employee.new(name, salary, self)
+    end
+    
+    def self.all_departments
+      self.all.map {|manager| manager.department}
+    end
+    
+    def self.manager_ages
+      self.all.sum {|manager| manager.age}
+    end
+    
+    def self.average_age
+      manager_ages.to_f / @@all.length.to_f
+    end
+    
+    def self.all
+      @@all
+    end
+    
+    
+  end
+  
--- run.rb
+++ run.rb
@@ -2,8 +2,12 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
-
-#Test your code here
+michael = Manager.new("Michael Scott", "Sales", 40)
+jim = Employee.new("Jim Halpert", 1000, michael)
+creed = Employee.new("Creed", 2000, michael)
+andy = Employee.new("Andy Bernard", 1400, michael)
+dwight = Employee.new("Dwight Schrute", 900, michael)
+stanley = Employee.new("Stanley", 3000, michael)
 
 
 binding.pry
