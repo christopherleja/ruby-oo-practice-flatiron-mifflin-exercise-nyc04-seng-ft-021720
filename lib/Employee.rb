85a3016ac51a7ae6e44db48a095bc766e332b032
Christopher Leja <christopher.leja@gmail.com>
2020-02-24 18:28:30 +0000

    Automatically backed up by Learn

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,40 @@
 class Employee
-
+  
+  attr_accessor :name, :salary, :manager
+  
+  @@all = []
+  
+  def initialize(name, salary, manager)
+  @name = name
+  @salary = salary
+  @manager = manager
+  @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def manager_name
+    self.manager.name
+  end
+  
+  def self.paid_over(num)
+    self.all.select {|employee| employee.salary > num}    
+  end
+  
+  def find_by_department(department)
+    self.all.find {|employee| employee.manager.department}
+  end
+  
+  def tax_bracket
+    salary = self.salary
+    bracket = @@all.select do |employee| 
+      if employee.salary >= (salary - 1000) && employee.salary <= (salary + 1000)
+      employee
+      end
+    end
+    bracket
+  end
+  
 end
